a69ef686dd8cf5569b20cfb85d18b6ce1a33a613
slimit75 <limitspeed75@gmail.com>
2021-07-08 18:26:43 -0400

    Remove redundant includes.

--- src/plugin.cpp
+++ src/plugin.cpp
@@ -7,17 +7,6 @@
 #include "XPLMUtilities.h"
 #include "XPLMPlugin.h"
 
-#if IBM
-    #include <windows.h>
-#endif
-#if LIN
-    #include <GL/gl.h>
-#elif __GNUC__
-    #include <OpenGL/gl.h>
-#else
-    #include <GL/gl.h>
-#endif
-
 // Error if compiling with wrong SDK
 #ifndef XPLM300
     #error This is made to be compiled against the XPLM300 SDK
